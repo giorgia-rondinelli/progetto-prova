f4ceffcf9fb1914e9b9f4b13f55d4375fd230149
Daniele206 <danieledeieso2004@gmail.com>
2024-06-13 12:20:16 +0200

    [add] cartoons 3d seeder

--- database/seeders/cartoonTableSeeder.php
+++ database/seeders/cartoonTableSeeder.php
@@ -30,5 +30,22 @@ class cartoonTableSeeder extends Seeder
             // dd($newCartoon);
             $newCartoon->save();
         }
+
+        $data_2_str = file_get_contents('https://api.sampleapis.com/cartoons/cartoons3D');
+        $data_2 = json_decode($data_2_str);
+        // dd($data);
+
+        foreach ($data_2 as $cartoon) {
+            $newCartoon = new Cartoon();
+            $newCartoon->title = $cartoon->title;
+            $newCartoon->slug = Helper::generateSlug($newCartoon->title, Cartoon::class);
+            $newCartoon->year = $cartoon->year;
+            $newCartoon->rating = $cartoon->rating;
+            $newCartoon->runtime_in_minutes = $cartoon->runtime_in_minutes;
+            $newCartoon->episodes = $cartoon->episodes;
+            $newCartoon->image = $cartoon->image;
+            // dd($newCartoon);
+            $newCartoon->save();
+        }
     }
 }
